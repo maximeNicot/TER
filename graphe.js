9ae2901c00b308c95d118a0c89e424304a969212
maximeNicot <maximenicot@outlook.com>
2019-04-01 23:14:07 +0200

    work in wamp, Json file read correctly

--- graphe.js
+++ graphe.js
@@ -1,63 +1,55 @@
-/*function jsonTest(){
-	d3.json("users.json").then(function(error, data){
-					if(error){
-						console.warn("erreur");
-						console.warn(error);
-						return;
-					}
-					d=data;
-			});
-			return "OK";
-}
-jsonTest();*/
+var details;
+d3.json("\\TER\\data.json").then(function(data){
+  details = data;
 
-var width = 600;
-var height = 500;
-var radius = 200;
-var colors = d3.scaleOrdinal(d3.schemeAccent);
 
-var svg = d3.select("body").append("svg") 
-	.attr("width", width)
-	.attr("height",height);
-	
-/*Le fichier json*/
-var details = [{id:"A", number:8}, {id:"C", number:21},
-	{id:"B", number:15}, {id:"E", number:2},
-	{id:"D", number:11}, {id:"F", number:6}];
-				
-var data = d3.pie().sort(null).value(function(d){return d.number;})
-(details);
+	var width = 600;
+	var height = 500;
+	var radius = 200;
+	var colors = d3.scaleOrdinal(d3.schemeAccent);
 
-var arc = d3.arc()
-	.innerRadius(radius - 100) /*Ici pour transformer en pie ou anneau*/
-	.outerRadius(radius)
-	.padAngle(.05)
-	.padRadius(50);
-	
+	var svg = d3.select("body").append("svg") 
+		.attr("width", width)
+		.attr("height",height);
 
-var sections = svg.append("g")
-	.attr("transform", "translate(250 , 250)")
-	.selectAll("path").data(data);
 
-sections.enter()
-	.append("path")
-	.attr("d",arc)
-	.on("mouseover",function(d){
-		d3.select(this).style("stroke", "black");
-	})
-	.on("mouseleave",function(d){
-		d3.select(this).style("stroke", "white");
-	})
-	.attr("fill", function(d){return colors (d.data.number);});
-	
-	
-	
-/*Pour afficher les number dedans*/
-var content = d3.select("g").selectAll("text").data(data);
+	//console.log(details);
 
-content.enter().append("text").each(function(d){
-	var center = arc.centroid(d);
-	d3.select(this).attr("x", center[0]).attr("y", center[1])
-					.text(d.data.number);
-	})
-	
+	var data = d3.pie().sort(null).value(function(d){return d.number;})
+	(details);
+
+	var arc = d3.arc()
+		.innerRadius(radius - 100) /*Ici pour transformer en pie ou anneau*/
+		.outerRadius(radius)
+		.padAngle(.05)
+		.padRadius(50);
+		
+
+	var sections = svg.append("g")
+		.attr("transform", "translate(250 , 250)")
+		.selectAll("path").data(data);
+
+	sections.enter()
+		.append("path")
+		.attr("d",arc)
+		.on("mouseover",function(d){
+			d3.select(this).style("stroke", "black");
+		})
+		.on("mouseleave",function(d){
+			d3.select(this).style("stroke", "white");
+		})
+		.attr("fill", function(d){return colors (d.data.number);});
+		
+		
+		
+	/*Pour afficher les number dedans*/
+	var content = d3.select("g").selectAll("text").data(data);
+
+	content.enter().append("text").each(function(d){
+		var center = arc.centroid(d);
+		d3.select(this).attr("x", center[0]-30).attr("y", center[1])
+						.text(d.data.id); // ici id ou number ou autre chose 
+		})
+		
+
+});
